10224af0937350b3d2220f6631875887ccd03297
andreslargo <felipeflogxd>
2019-11-13 18:45:06 -0500

    Algo que acabo de hacer ahora mismo

--- src/app/components/listapeticiones/lista-peticiones.component.ts
+++ src/app/components/listapeticiones/lista-peticiones.component.ts
@@ -8,11 +8,19 @@ import { ListauxService } from '../../services/listaux.service'
 export class ListaPeticionesComponent implements OnInit {
   type: string;
   Peticiones;
+  public form: DynamicFormGroup<Soli>;
   constructor(private service: ListauxService) { 
     this.type = "student";  
   }
 
   async ngOnInit() {
+    this.form = this.fb.group(Soli, {
+      date: '', datetime: '', name: '', labid: '',
+      asign: '', teacherid: '', iduser: parseInt(await localStorage.getItem("logged")), facultyid: '',
+      students: '', cantmachines: '', cantconsume: '',
+      observation: '', usemachines: false,
+      useconsume: false, guide: false, status: 0
+    });
     this.service.listUser(await localStorage.getItem("logged")).subscribe(data => {
       this.Peticiones = data;
     })
