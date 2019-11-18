a6ef9ec1a0a38482f014394d244b7c9e00265426
Andrés Largo <felipeflogxd@gmail.com>
2019-11-17 23:00:25 -0500

    Update listaux.service.ts

--- src/app/services/listaux.service.ts
+++ src/app/services/listaux.service.ts
@@ -9,20 +9,20 @@ export class ListauxService {
   list = () => {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json'); // Hacemos saber que es jSON
-    return this.http.get("http://localhost:10030/labrequest/list", {headers: headers}) // pETICIÓN GET
+    return this.http.get("http://localhost:10070/labrequest/list", {headers: headers}) // pETICIÓN GET
   }
 
   accept = (id, model) => {
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json'); 
-    return this.http.put("http://localhost:10030/labrequest/edit/" + id, model, { responseType: 'text'  })
+    return this.http.put("http://localhost:10070/labrequest/edit/" + id, model, { responseType: 'text'  })
   }
 
   listUser = (id) => {
     console.log(id)
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json'); // Hacemos saber que es jSON
-    return this.http.get("http://localhost:10030/labrequest/userlist/" + id, { headers: headers }) // pETICIÓN GET
+    return this.http.get("http://localhost:10070/labrequest/userlist/" + id, { headers: headers }) // pETICIÓN GET
   }
 
 
